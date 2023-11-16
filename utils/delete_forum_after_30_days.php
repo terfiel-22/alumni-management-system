28d2b22b998ae8e9dd480f2dff6f86f406ed7978
terfiel-22 <dietherfiel@gmail.com>
2023-11-16 20:49:01 +0800

    Delete forums older than 30 days

---
+++ utils/delete_forum_after_30_days.php
@@ -0,0 +1,14 @@
+<?php 
+
+function deleteOldForums($conn) {
+    // Calculate the date 30 days ago
+    $thirtyDaysAgo = date('Y-m-d H:i:s', strtotime('-30 days'));
+
+    // Query to delete posts older than 30 days
+    $deleteQuery = "DELETE FROM forum_topics WHERE date_created < '$thirtyDaysAgo'";
+
+    // Execute the query
+    if ($conn->query($deleteQuery) === TRUE) {
+        echo "Forums older than 30 days have been deleted.";
+    }  
+}
