952d52a99328f4358fc1820bb632eff31b6d524a
terfiel-22 <dietherfiel@gmail.com>
2023-11-17 19:45:39 +0800

    Move Alumni Officers on Admin Access

--- admin/navbar.php
+++ admin/navbar.php
@@ -15,14 +15,14 @@
 				<a href="index.php?page=gallery" class="nav-item nav-gallery"><span class='icon-field'><i class="fa fa-image"></i></span> Gallery </a>
 				<a href="index.php?page=courses" class="nav-item nav-courses"><span class='icon-field'><i class="fa fa-list"></i></span> Course List</a>
 				<a href="index.php?page=alumni" class="nav-item nav-alumni"><span class='icon-field'><i class="fa fa-users"></i></span> Alumni List</a>
-				<a href="index.php?page=alumni_officers" class="nav-item nav-alumni_officers"><span class='icon-field'><i class="fa fa-users"></i></span> Alumni Officers</a>
 				<a href="index.php?page=jobs" class="nav-item nav-jobs"><span class='icon-field'><i class="fa fa-briefcase"></i></span> Career</a> 
 				<a href="index.php?page=forums" class="nav-item nav-forums"><span class='icon-field'><i class="fa fa-comments"></i></span> Program & Events </a>
 				<?php if($_SESSION['login_type'] == 1): ?>
 				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Users</a>
 				<a href="index.php?page=documents" class="nav-item nav-documents"><span class='icon-field'><i class="fa fa-file"></i></span> Documents</a>
+				<a href="index.php?page=alumni_officers" class="nav-item nav-alumni_officers"><span class='icon-field'><i class="fa fa-users"></i></span> Alumni Officers</a>
 				<a href="index.php?page=site_settings" class="nav-item nav-site_settings"><span class='icon-field'><i class="fa fa-cogs"></i></span> System Settings</a>
-			<?php endif; ?>
+				<?php endif; ?>
 		</div>
 
 </nav>
