13aff575e875e58c88170a2551dc7dfadf9e5f49
terfiel-22 <dietherfiel@gmail.com>
2023-11-18 09:40:36 +0800

    Add shortcut key

--- login.php
+++ login.php
@@ -15,33 +15,33 @@
 </div>
 
 <style>
-	#uni_modal .modal-footer{
-		display:none;
+	#uni_modal .modal-footer {
+		display: none;
 	}
 </style>
 
 <script>
-	$('#login-frm').submit(function(e){
+	$('#login-frm').submit(function(e) {
 		e.preventDefault()
-		$('#login-frm button[type="submit"]').attr('disabled',true).html('Logging in...');
-		if($(this).find('.alert-danger').length > 0 )
+		$('#login-frm button[type="submit"]').attr('disabled', true).html('Logging in...');
+		if ($(this).find('.alert-danger').length > 0)
 			$(this).find('.alert-danger').remove();
 		$.ajax({
-			url:'admin/ajax.php?action=login2',
-			method:'POST',
-			data:$(this).serialize(),
-			error:err=>{
+			url: 'admin/ajax.php?action=login2',
+			method: 'POST',
+			data: $(this).serialize(),
+			error: err => {
 				console.log(err)
-		$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
+				$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
 
 			},
-			success:function(resp){
-				if(resp == 1){
-					location.href ='<?php echo isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php?page=home' ?>';
-				}else if(resp == 2){
+			success: function(resp) {
+				if (resp == 1) {
+					location.href = '<?php echo isset($_GET['redirect']) ? $_GET['redirect'] : 'index.php?page=home' ?>';
+				} else if (resp == 2) {
 					$('#login-frm').prepend('<div class="alert alert-danger">Your account is not yet verified.</div>')
 					$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
-				}else{
+				} else {
 					$('#login-frm').prepend('<div class="alert alert-danger">Email or password is incorrect.</div>')
 					$('#login-frm button[type="submit"]').removeAttr('disabled').html('Login');
 				}
